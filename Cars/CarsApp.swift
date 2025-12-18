3dbc1bc16465bebc8fb3684a1d5d3714b0e58a4f
Vic Hagens <vic.hagens@iMacO.local>
2025-12-18 11:23:12 +0100

    first commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by Vic Hagens on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
